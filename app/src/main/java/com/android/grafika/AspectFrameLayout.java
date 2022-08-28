2518e9455be23861c4d8eb2512a62d817f20bce0
Your Name <you@example.com>
2022-08-28 12:36:57 +0200

    zaz

--- app/src/main/java/com/android/grafika/AspectFrameLayout.java
+++ app/src/main/java/com/android/grafika/AspectFrameLayout.java
@@ -92,8 +92,7 @@ public class AspectFrameLayout extends FrameLayout {
                 initialWidth += horizPadding;
                 initialHeight += vertPadding;
                 widthMeasureSpec = MeasureSpec.makeMeasureSpec(initialWidth, MeasureSpec.EXACTLY);
-                heightMeasureSpec = MeasureSpec.makeMeasureSpec(initialHeight, MeasureSpec.EXACTLY);
-            }
+                
         }
 
         //Log.d(TAG, "set width=[" + MeasureSpec.toString(widthMeasureSpec) +
